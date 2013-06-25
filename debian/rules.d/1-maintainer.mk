ubuntustudio-kernel/ubuntu-raring-lowlatency

UBUNTU: Look for releases marked Lowlatency instead of Ubuntu
The printchanges target needs to look for Lowlatency instead of Ubuntu
when searching for changes to print since the last release.

Signed-off-by: Luke Yelavich <themuso@ubuntu.com>
Signed-off-by: Andy Whitcroft <apw@canonical.com>

debian/rules.d/1-maintainer.mk
@@ -91,7 +91,7 @@ printenv:
 prev_release="$(release)-$(prev_revision)"
 printchanges:
 	@baseCommit=$$(git log --pretty=format:'%H %s' | \
-		awk '/UBUNTU: '".*Ubuntu-$(prev_release)"'$$/ { print $$1; exit }'); \
+		awk '/UBUNTU: '".*Lowlatency-$(prev_release)"'$$/ { print $$1; exit }'); \
 		git log "$$baseCommit"..HEAD | \
 		perl -w -f $(DROOT)/scripts/misc/git-ubuntu-log $(ubuntu_log_opts)
 

ubuntustudio-kernel/ubuntu-saucy-lowlatency

debian/rules.d/1-maintainer.mk
@@ -90,7 +90,7 @@ printenv:
 
 printchanges:
 	@baseCommit=$$(git log --pretty=format:'%H %s' | \
-		awk '/UBUNTU: '".*Ubuntu-$(prev_fullver)"'$$/ { print $$1; exit }'); \
+		awk '/UBUNTU: '".*Lowlatency-$(prev_fullver)"'$$/ { print $$1; exit }'); \
 		git log "$$baseCommit"..HEAD | \
 		perl -w -f $(DROOT)/scripts/misc/git-ubuntu-log $(ubuntu_log_opts)
 